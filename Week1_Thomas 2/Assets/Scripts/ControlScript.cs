fd31748a3d974dbd45aa934d3c4ef1188b6a1db8
Addison Thomas <addison.thomas@du.edu>
2020-04-15 10:50:10 -0600

    Initial Commit, Thomas files addeD

---
+++ Week1_Thomas 2/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlScript : MonoBehaviour {
+
+	public Transform collect;
+	public Transform avoid;
+
+	private float maxItems = 50;
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i<maxItems; i++){
+			Vector2 randPosition = new Vector2(Random.Range(-50,50),Random.Range(-50,50));
+			if(Random.value<.3f){
+				Instantiate (collect, randPosition,Quaternion.identity);
+			}else{
+				Instantiate (avoid, randPosition,Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Week1_Thomas 2/Assets/Scripts/PlayerMove1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove1 : MonoBehaviour {
+
+	public float hspeed =6;
+	public float vspeed=6;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h=Input.GetAxis("Horizontal");
+
+		float v=Input.GetAxis("Vertical");
+
+		transform.Translate(new Vector3(h*hspeed*Time.deltaTime,v*vspeed*Time.deltaTime,0));
+
+	}
+}
